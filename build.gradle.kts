172e24408e59cae31f276402ce80ef987d5f620a
Shuji <marcellalter@gmail.com>
2024-07-08 11:29:45 +0200

    add command

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,17 @@
-import xyz.jpenilla.resourcefactory.bukkit.BukkitPluginYaml
-
 plugins {
-  `java-library`
-  id("io.papermc.paperweight.userdev") version "1.7.1"
-  id("xyz.jpenilla.run-paper") version "2.3.0"
-  id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.1.1"
+	`java-library`
+	id("io.papermc.paperweight.userdev") version "1.7.1"
+	id("xyz.jpenilla.run-paper") version "2.3.0"
 }
 
 group = "me.shuji.joinmessage"
 version = "1.0.0"
 description = "joinmessage"
 
+
 java {
-  // Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
-  toolchain.languageVersion = JavaLanguageVersion.of(21)
+	// Configure the java toolchain. This allows gradle to auto-provision JDK 21 on systems that only have JDK 11 installed for example.
+	toolchain.languageVersion = JavaLanguageVersion.of(21)
 }
 
 // 1)
@@ -30,34 +28,33 @@ tasks.assemble {
  */
 
 dependencies {
-  paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
+	paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
 }
 
 tasks {
-  compileJava {
-    // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-    // See https://openjdk.java.net/jeps/247 for more information.
-    options.release = 21
-  }
-  javadoc {
-    options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-  }
+	compileJava {
+		options.release = 21
+	}
+	javadoc {
+		options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+	}
 
-  // Only relevant when going with option 2 above
-  /*
-  reobfJar {
-    // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
-    // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
-    outputJar = layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar")
-  }
-   */
+	// Only relevant when going with option 2 above
+	/*
+	reobfJar {
+			// This is an example of how you might change the output location for reobfJar. It's recommended not to do this
+			// for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
+			outputJar = layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar")
+	}
+		*/
 }
 
 // Configure plugin.yml generation
 // - name, version, and description are inherited from the Gradle project.
-bukkitPluginYaml {
-  main = "me.shuji.joinmessage.Main"
-  load = BukkitPluginYaml.PluginLoadOrder.STARTUP
-  author = "Shuji"
-  apiVersion = "1.21"
-}
+/*bukkitPluginYaml {
+	main = "me.shuji.joinmessage.Main"
+	load = BukkitPluginYaml.PluginLoadOrder.STARTUP
+	author = "Shuji"
+	apiVersion = "1.21"
+
+}*/
